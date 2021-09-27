e75b8551f6440f1b55a27c9ae15ae1f06578dfdb
Diko Hary Adhanto <73458616+dikoharyadhanto@users.noreply.github.com>
2021-09-27 18:28:23 +0700

    Update part 1.js

--- part 1.js
+++ part 1.js
@@ -1,15 +1,15 @@
-var dataset = ee.ImageCollection('LANDSAT/LC08/C01/T1')
-                  .filterBounds(jakarta)
-                  .filterDate('2017-06-01', '2017-07-31');
-print(dataset);
+var dataset = ee.ImageCollection('LANDSAT/LC08/C01/T1') // buat nentuin image-nya. Kode image dicatet dulu di menu pencarian citra
+                  .filterBounds(geometry) //fungsi untuk memfilter pencarian geomer atas sebuah dataset
+                  .filterDate('2021-06-01', '2021-09-27'); // fungsi untuk pencarian berdasarkan waktu
+print(dataset); //fungsi memunculkan dataset layer
 
-var single = ee.Image('LANDSAT/LC08/C01/T1/LC08_122064_20170617')
+var single = ee.Image('LANDSAT/LC08/C01/T1/LC08_122064_20210916') //buat nampilin image-nya
             
-var RGBTrue = single.select(['B4', 'B3', 'B2']);
+var RGBTrue = single.select(['B4', 'B3', 'B2']); //buat nampilin composite band True color
 
 //memunculkan citra
-var RGBparam = { min: 0, max: 30000,};
-Map.addLayer(RGBTrue, RGBparam, 'TRUE');
+var RGBparam = { min: 0, max: 20000,}; //mau nampilin citra dengan nilai pixel rentang berapa sampe berapa
+Map.addLayer(RGBTrue, RGBparam, 'Nama_Citra-nya');
 
 
 
